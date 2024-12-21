22e8cb87311630bfd73c4c56eb5492e6ffcafe0e
tlnghia2231 <22115053122231@sv.ut.udn.vn>
2024-12-21 09:10:38 +0700

    Up code

---
+++ Github/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+<<<<<<< HEAD
+
+        Toast.makeText(this, "Abc", Toast.LENGTH_SHORT).show();
+        System.out.printf("Hello, %s!", "world lan 3");
+
+=======
+        System.out.printf("Hello, %s!", "world lan 2");
+>>>>>>> f9e00a882cc46e690b75754b86de88b79f99c533
+    }
+}
---
+++ Github/app/src/main/java/com/example/myapplication/Nhanh2.java
@@ -0,0 +1,8 @@
+package com.example.myapplication;
+
+import java.io.PrintStream;
+import android.widget.Toast;
+
+public class Nhanh2 {
+    System.out.printf("Hello, %s!", "world");
+}
